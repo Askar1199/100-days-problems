0c3f9cda449fbf280f19bf283dd0e2c524e77ebb
Mohammed Askar <mdaskar40@gmail.com>
2024-03-28 15:43:37 +0530

    first commit

---
+++ day_1/arrayWrapper.js
@@ -0,0 +1,19 @@
+/*
+Example 1:
+
+Input: nums = [[1,2],[3,4]], operation = "Add"
+Output: 10
+Explanation:
+const obj1 = new ArrayWrapper([1,2]);
+const obj2 = new ArrayWrapper([3,4]);
+obj1 + obj2; // 10
+*/
+
+const wrapper = (arr, ope) => {
+  let val = arr.flat();
+ 
+
+  
+};
+
+console.log(wrapper([[1,2],[3,4]],'add'))
